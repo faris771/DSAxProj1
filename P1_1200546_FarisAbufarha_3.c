ce6cf93fb446cf8ebba2c5ddb5cd214505b87ae0
Faris Abufarha <farisabufarha33@gmai.com>
2022-04-21 10:24:39 +0300

    Initial commit

---
+++ P1_1200546_FarisAbufarha_3.c
@@ -0,0 +1,49 @@
+/**
+*
+ *
+ * Author: Faris Abufarha
+ * SEC:3
+ * ID: 1200546
+ *
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+typ
+#define null NULL
+#define ll long long
+
+typedef struct Bus{
+
+    int busID;
+    //could be either string or int
+
+
+
+
+}Bus;
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
